a3238911ef6e572b50ba73bbb3c668759d195808
barikashutosh3 <51753721+barikashutosh3@users.noreply.github.com>
2023-01-21 14:14:58 +0530

    Removed Footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,11 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
